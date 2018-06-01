a4faff08f65ae3aba51106f439852c6bcc023cba
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2018-06-01 17:18:30 +0200

    basic frontend (technical prototype)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,3 @@
 require('../sass/app.scss');
-require('../../node_modules/chess.js/chess.min.js');
-require('../../node_modules/chessboard/chessboard-min.js');
+require('../sass/chessboard.scss');
+require('./chessboard.js');
