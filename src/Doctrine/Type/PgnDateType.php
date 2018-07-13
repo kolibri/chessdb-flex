e85592358cd74496907ee8d1de4dcf3fdfb87226
Lukas Sadzik <lukas.sadzik@sensiolabs.de>
2018-07-13 16:07:41 +0200

    Added some tests and travis config

--- src/Doctrine/Type/PgnDateType.php
+++ src/Doctrine/Type/PgnDateType.php
@@ -8,22 +8,27 @@ use Doctrine\DBAL\Types\Type;
 
 class PgnDateType extends Type
 {
-    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    /**
+     * @param PgnDate $value
+     * @param AbstractPlatform $platform
+     * @return string
+     */
+    public function convertToDatabaseValue($value, AbstractPlatform $platform): string
     {
         return $value->toString();
     }
 
-    public function convertToPHPValue($value, AbstractPlatform $platform)
+    public function convertToPHPValue($value, AbstractPlatform $platform): PgnDate
     {
         return PgnDate::fromString($value);
     }
 
-    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform)
+    public function getSQLDeclaration(array $fieldDeclaration, AbstractPlatform $platform): string
     {
         return 'TINYTEXT';
     }
 
-    public function getName()
+    public function getName(): string
     {
         return 'pgn_date';
     }
---
+++ tests/Unit/Twig/ChessExtensionTest.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace App\Tests\Unit\Twig;
+
+use App\Twig\ChessExtension;
+use PHPUnit\Framework\TestCase;
+
+class ChessExtensionTest extends TestCase
+{
+    public function testRenderStringToDivWorks()
+    {
+        $pgn = 'sample_pgn';
+    }
+}
