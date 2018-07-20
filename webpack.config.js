8a5fc4f063a87b20f461c796efbb26372569468e
ko <entengelb@gmail.com>
2018-07-20 05:03:52 +0200

    WIP update to new chessboard. encore is fucking around

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,11 @@ var Encore = require('@symfony/webpack-encore');
 Encore
     .setOutputPath('public/build/')
     .setPublicPath('/build')
+    .enableReactPreset()
     .cleanupOutputBeforeBuild()
     .enableSourceMaps(!Encore.isProduction())
-    .addEntry('app', './assets/js/app.js')
     .enableSassLoader()
-    .cleanupOutputBeforeBuild()
+    .addEntry('app', './assets/js/app.jsx')
 ;
 
 module.exports = Encore.getWebpackConfig();
